fdfb59fc9eb7bd30b041a930e8c262351feca5a3
Igor Oganesian <igor.oganesian@gmail.com>
2023-08-15 19:18:31 -0400

    added post route validation and schema

--- routes/shipments.js
+++ routes/shipments.js
@@ -5,6 +5,8 @@ const { BadRequestError } = require("../expressError");
 const router = new express.Router();
 
 const { shipProduct } = require("../shipItApi");
+const jsonschema = require("jsonschema");
+const shippingSchema = require("../schemas/shippingSchema.json");
 
 /** POST /ship
  *
@@ -15,9 +17,13 @@ const { shipProduct } = require("../shipItApi");
  */
 
 router.post("/", async function (req, res, next) {
-  if (req.body === undefined) {
-    throw new BadRequestError();
+  const result = jsonschema.validate(
+    req.body, shippingSchema, {required: true});
+  if (!result.valid) {
+    const errs = result.errors.map(err => err.stack);
+    throw new BadRequestError(errs);
   }
+
   const { productId, name, addr, zip } = req.body;
   const shipId = await shipProduct({ productId, name, addr, zip });
   return res.json({ shipped: shipId });
